8395a605a95150337eef3292ebaeafb1bb578a27
Allison Blumenthal <blumen.allison@gmail.com>
2022-11-19 15:11:26 -0500

    working on view Authors

--- pages/viewAuthor.js
+++ pages/viewAuthor.js
@@ -4,7 +4,7 @@ import renderToDOM from '../utils/renderToDom';
 const viewAuthor = (obj) => {
   clearDom();
 
-  const domString = `<div class="card" style="width: 18rem;">
+  let domString = `<div class="card" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">${obj.first_name} ${obj.last_name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${obj.email}</h6>
@@ -15,22 +15,21 @@ const viewAuthor = (obj) => {
       </div>
     </div>`;
 
-  // obj.authorBooks.map((book) => {
-  //   domString += `
-  //       <div class="card">
-  //         <img class="card-img-top" src=${obj.authorBooks.image} alt=${obj.authorBooks.title} style="height: 400px;">
-  //         <div class="card-body" style="height: 180px;">
-  //           <h5 class="card-title">${obj.authorBooks.title}</h5>
-  //             <p class="card-text bold">${obj.authorBooks.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${obj.authorBooks.price}` : `$${obj.authorBooks.price}`}</p>
-  //             <hr>
-  //             <i class="btn btn-success fas fa-eye" id="view-book-btn--${obj.authorBooks.firebaseKey}"></i>
-  //             <i id="edit-book-btn--${obj.authorBooks.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-  //             <i id="delete-book-btn--${obj.authorBooks.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
-  //         </div>
-  //       </div>`;
-  // });
-
-  renderToDOM('#view', domString);
+  obj.authorBooksArr.map((book) => {
+    domString += `
+        <div class="card">
+          <img class="card-img-top" src=${book.image} alt=${book.title} style="height: 400px;">
+          <div class="card-body" style="height: 180px;">
+            <h5 class="card-title">${book.title}</h5>
+              <p class="card-text bold">${book.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${book.price}` : `$${book.price}`}</p>
+              <hr>
+              <i class="btn btn-success fas fa-eye" id="view-book-btn--${book.firebaseKey}"></i>
+              <i id="edit-book-btn--${book.firebaseKey}" class="fas fa-edit btn btn-info"></i>
+              <i id="delete-book-btn--${book.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+          </div>
+        </div>`;
+    return renderToDOM('#view', domString);
+  });
 };
 
 export default viewAuthor;
